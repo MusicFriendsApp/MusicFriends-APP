021f964097e9fdd1adef7dd0a86bf9f50a0fca96
DarkOwn3r <pablosntojd@gmail.com>
2024-04-23 14:34:43 +0100

    feat: login to spotify

--- .history/Front-End/src/pages/Home/Home_20240423140304.jsx
+++ .history/Front-End/src/pages/Home/Home_20240423140304.jsx
@@ -1,14 +1,10 @@
 import './Home.css'
-import { loginSpotify } from '../../services/loginSpotify'
 
 const Home = () => {
-
-
-
   return (
     <div id='homepage'>
       <h1>This is the home page</h1>
-      <button onClick={loginSpotify}>Log In</button>
+      <button>Log In</button>
     </div>
   )
 }
--- .history/Front-End/src/pages/Home/Home_20240423141049.jsx
+++ .history/Front-End/src/pages/Home/Home_20240423141049.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div id='homepage'>
       <h1>This is the home page</h1>
-      <button onClick={loginSpotify}>Log In</button>
+      <button onClick={loginSpotify()}>Log In</button>
     </div>
   )
 }

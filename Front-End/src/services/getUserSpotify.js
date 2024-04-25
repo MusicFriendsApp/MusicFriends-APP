ac34bf97cbf7af7a35eb566bf5481afea28a5d02
DarkOwn3r <pablosntojd@gmail.com>
2024-04-25 10:13:25 +0100

    fix: token renamed to access_token on fetchUser

--- Front-End/src/services/getUserSpotify.js
+++ Front-End/src/services/getUserSpotify.js
@@ -1,13 +1,12 @@
 export default async function getUserSpotify() {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem('access_token')
     async function fetchProfile(token) {
       const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
       });
       const profile = await result.json();
       return profile
-    } 
-    console.log('this is the user profile')
+    }
     const profile = await fetchProfile(token)
     return profile
 }

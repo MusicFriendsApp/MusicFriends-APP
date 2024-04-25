955e0e69bca9c5d0b32f07f617116c6ab00c8305
tajamajaka <tajamajaka@gmail.com>
2024-04-25 15:02:06 +0100

    modifying getUserTopArtist...

--- Front-End/src/pages/Home/Home.jsx
+++ Front-End/src/pages/Home/Home.jsx
@@ -2,8 +2,9 @@ import { loginSpotify } from "../../services/loginSpotify"
 import "./Home.css"
 import { useEffect, useState } from "react"
 import { createUser } from '../../services/user'
-import {getUserSpotify} from '../../services/getUserSpotify' 
-import { getUserTopArtist } from '../../services/user'
+import { getUserSpotify } from '../../services/getUserSpotify' 
+import { getUserTopArtist } from '../../services/getUserTopArtist'
+import { userTopTenArtist } from '../../services/user'
 
 const Home = () => {
   useEffect(() => {
@@ -33,16 +34,14 @@ const Home = () => {
 
     const getUserTopArtistData = async () => {
       const {items} = await getUserTopArtist()
-      const data = items.map((artist)=>{
+      const data = items.map(( artist )=>{
         return artist.name, artist.id
       })
+      console.log(items)
 
-      userTopTenArtist( artist.name, artist.id )
     }
-
-
-
-    getUserTopArtist()
+    
+    getUserTopArtistData()
 
 
   }, [token])

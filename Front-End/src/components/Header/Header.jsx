b8ebc8b72575acfc9ebbb85a1d623c598345a920
DarkOwn3r <pablosntojd@gmail.com>
2024-04-29 12:30:23 +0100

    feat: added some styling to the home page, header and footer

--- Front-End/src/components/Header/Header.jsx
+++ Front-End/src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ export default function Header() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };return (
-    <AppBar position="static">
+    <AppBar position="sticky">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />

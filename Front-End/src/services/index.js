93e70dea58c842f6f06c8cacb793c54d512de2cb
DarkOwn3r <pablosntojd@gmail.com>
2024-05-02 16:15:29 +0100

    api baseurl changed to localhost

--- Front-End/src/services/index.js
+++ Front-End/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://spotifyfriends-app.onrender.com:3000/api'
+    baseURL: 'https://localhost:3000/api'
 })
 
 export default api

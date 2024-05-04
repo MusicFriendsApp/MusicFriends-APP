b0ddace87f6073272ae0523b86d20145d99c1c64
Pablo Santana <pablosntojd@gmail.com>
2024-05-04 13:36:11 +0100

    fix: new login callback route to music-friends.netlify.app/loading

--- Front-End/src/services/loginSpotify.js
+++ Front-End/src/services/loginSpotify.js
@@ -25,7 +25,7 @@ export async function loginSpotify() {
       const params = new URLSearchParams()
       params.append('client_id', clientId)
       params.append('response_type', 'code')
-      params.append('redirect_uri', 'https://spotify-friends.netlify.app/loading')
+      params.append('redirect_uri', 'https://music-friends.netlify.app/loading')
       params.append('scope', 'user-read-private user-read-email user-read-currently-playing user-top-read')
       params.append('code_challenge_method', 'S256')
       params.append('code_challenge', challenge)
@@ -60,7 +60,7 @@ export async function loginSpotify() {
         params.append('client_id', clientId)
         params.append('grant_type', 'authorization_code')
         params.append('code', code)
-        params.append('redirect_uri', 'https://spotify-friends.netlify.app/loading')
+        params.append('redirect_uri', 'https://music-friends.netlify.app/loading')
         params.append('code_verifier', verifier)
         
         const result = await fetch('https://accounts.spotify.com/api/token', {
